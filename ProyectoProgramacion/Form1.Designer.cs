b62c3b4d1a3f3967235d90e91819d4879bc8372b
holahola11 <holamundi@email.com>
2025-11-09 19:09:01 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoProgramacion/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace ProyectoProgramacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.txtDatos = new System.Windows.Forms.TextBox();
+            this.lblDistancia = new System.Windows.Forms.Label();
+            this.barraDistancia = new System.Windows.Forms.ProgressBar();
+            this.btnEncenderLaser = new System.Windows.Forms.Button();
+            this.btnApagarLaser = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnConectar.FlatAppearance.BorderSize = 0;
+            this.btnConectar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConectar.Location = new System.Drawing.Point(15, 68);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 2;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = false;
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnDesconectar.FlatAppearance.BorderSize = 0;
+            this.btnDesconectar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDesconectar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnDesconectar.Location = new System.Drawing.Point(152, 68);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(118, 23);
+            this.btnDesconectar.TabIndex = 3;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = false;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblEstado.Location = new System.Drawing.Point(72, 136);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(96, 16);
+            this.lblEstado.TabIndex = 4;
+            this.lblEstado.Text = "Desconectado";
+            // 
+            // txtDatos
+            // 
+            this.txtDatos.Location = new System.Drawing.Point(148, 116);
+            this.txtDatos.Name = "txtDatos";
+            this.txtDatos.Size = new System.Drawing.Size(121, 22);
+            this.txtDatos.TabIndex = 5;
+            // 
+            // lblDistancia
+            // 
+            this.lblDistancia.AutoSize = true;
+            this.lblDistancia.Location = new System.Drawing.Point(206, 39);
+            this.lblDistancia.Name = "lblDistancia";
+            this.lblDistancia.Size = new System.Drawing.Size(63, 16);
+            this.lblDistancia.TabIndex = 6;
+            this.lblDistancia.Text = "Distancia";
+            // 
+            // barraDistancia
+            // 
+            this.barraDistancia.Location = new System.Drawing.Point(148, 58);
+            this.barraDistancia.Name = "barraDistancia";
+            this.barraDistancia.Size = new System.Drawing.Size(182, 23);
+            this.barraDistancia.TabIndex = 7;
+            // 
+            // btnEncenderLaser
+            // 
+            this.btnEncenderLaser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnEncenderLaser.FlatAppearance.BorderSize = 0;
+            this.btnEncenderLaser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEncenderLaser.Location = new System.Drawing.Point(232, 52);
+            this.btnEncenderLaser.Name = "btnEncenderLaser";
+            this.btnEncenderLaser.Size = new System.Drawing.Size(161, 31);
+            this.btnEncenderLaser.TabIndex = 8;
+            this.btnEncenderLaser.Text = "Encender laser";
+            this.btnEncenderLaser.UseVisualStyleBackColor = false;
+            // 
+            // btnApagarLaser
+            // 
+            this.btnApagarLaser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnApagarLaser.FlatAppearance.BorderSize = 0;
+            this.btnApagarLaser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnApagarLaser.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnApagarLaser.Location = new System.Drawing.Point(7, 52);
+            this.btnApagarLaser.Name = "btnApagarLaser";
+            this.btnApagarLaser.Size = new System.Drawing.Size(168, 31);
+            this.btnApagarLaser.TabIndex = 9;
+            this.btnApagarLaser.Text = "Apagar laser";
+            this.btnApagarLaser.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Datos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(52, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Conexion Serial";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(93, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Lectura del Sensor";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Distancia detectada: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(137, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(145, 25);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Control Manual";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btnConectar);
+            this.panel1.Controls.Add(this.btnDesconectar);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lblEstado);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 227);
+            this.panel1.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panel2.Controls.Add(this.barraDistancia);
+            this.panel2.Controls.Add(this.txtDatos);
+            this.panel2.Controls.Add(this.lblDistancia);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(413, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(361, 227);
+            this.panel2.TabIndex = 18;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.btnEncenderLaser);
+            this.panel3.Controls.Add(this.btnApagarLaser);
+            this.panel3.Location = new System.Drawing.Point(175, 312);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(412, 100);
+            this.panel3.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.TextBox txtDatos;
+        private System.Windows.Forms.Label lblDistancia;
+        private System.Windows.Forms.ProgressBar barraDistancia;
+        private System.Windows.Forms.Button btnEncenderLaser;
+        private System.Windows.Forms.Button btnApagarLaser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ ProyectoProgramacion/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using MySql.Data.MySqlClient;
+
+namespace ProyectoProgramacion
+{
+    public partial class Form1 : Form
+    {
+        SerialPort puertoSerial;
+        string conexionMySQL = "Server=localhost;Database=Torreta_PA;Uid=root;Pwd=1234;";
+        bool conectado = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // .DataSource = SerialPort.GetPortNames(); 
+            // comboBaudios.SelectedIndex = 0;
+            btnDesconectar.Enabled = false;
+
+            string[] puertos = SerialPort.GetPortNames();
+
+            if (puertos.Length > 0)
+            {
+                string puerto = puertos[0]; // Usa el primer puerto encontrado
+                puertoSerial = new SerialPort(puerto, 9600); // Arduino usa 9600 baudios
+                lblEstado.Text = $"Puerto detectado: {puerto}";
+                lblEstado.ForeColor = Color.Green;
+            }
+            else
+            {
+                MessageBox.Show("No se detectó ningún puerto COM. Conecta el Arduino y reinicia el programa.");
+                lblEstado.Text = "Sin puerto detectado";
+                lblEstado.ForeColor = Color.Red;
+            }
+
+            try
+            {
+                using (var conexion = new MySqlConnection("Server=localhost;Database=Torreta_PA;Uid=root;Pwd=1234;"))
+                {
+                    conexion.Open();
+                    MessageBox.Show("Conexión a MySQL exitosa");
+                    conexion.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("❌ Error al conectar a MySQL: " + ex.Message);
+            }
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!conectado && puertoSerial != null)
+                {
+                    puertoSerial.DataReceived += PuertoSerial_DataReceived;
+                    puertoSerial.Open();
+                    conectado = true;
+
+                    lblEstado.Text = "Conectado al Arduino";
+                    lblEstado.ForeColor = Color.Green;
+
+                    btnConectar.Enabled = false;
+                    btnDesconectar.Enabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar: " + ex.Message);
+            }
+        }
+
+
+        private void PuertoSerial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = puertoSerial.ReadLine();
+                this.Invoke(new Action(() =>
+                {
+                    txtDatos.AppendText(data + Environment.NewLine);
+
+                    if (data.Contains("Distance"))
+                    {
+                        string[] partes = data.Split(' ');
+                        if (partes.Length >= 2 && int.TryParse(partes[1], out int distancia))
+                        {
+                            lblDistancia.Text = distancia + " cm";
+                            barraDistancia.Value = Math.Min(distancia, 100);
+                            try
+                            {
+                                using (var conexion = new MySql.Data.MySqlClient.MySqlConnection(conexionMySQL))
+                                {
+                                    conexion.Open();
+                                    string query = "INSERT INTO detecciones (distancia_cm, objetivo_detectado, respuesta_activada) VALUES (@distancia, @detectado, @respuesta)";
+                                    using (var comando = new MySql.Data.MySqlClient.MySqlCommand(query, conexion))
+                                    {
+                                        comando.Parameters.AddWithValue("@distancia", distancia);
+                                        comando.Parameters.AddWithValue("@detectado", distancia < 100); // TRUE si está cerca
+                                        comando.Parameters.AddWithValue("@respuesta", distancia < 100 ? "Laser" : "Ninguno");
+                                        comando.ExecuteNonQuery();
+                                    }
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Error al guardar en la base de datos: " + ex.Message);
+                            }
+                        }
+                    }
+                }));
+            }
+            catch { }
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            if (puertoSerial != null && puertoSerial.IsOpen)
+            {
+                puertoSerial.Close();
+                conectado = false;
+                lblEstado.Text = "Desconectado";
+                lblEstado.ForeColor = System.Drawing.Color.Red;
+
+                btnConectar.Enabled = true;
+                btnDesconectar.Enabled = false;
+            }
+        }
+
+        private void btnEncenderLaser_Click(object sender, EventArgs e)
+        {
+            if (puertoSerial != null && puertoSerial.IsOpen)
+                puertoSerial.Write("L"); // Envía comando al Arduino para encender láser
+        }
+
+        private void btnApagarLaser_Click(object sender, EventArgs e)
+        {
+            if (puertoSerial != null && puertoSerial.IsOpen)
+                puertoSerial.Write("l"); // Envía comando al Arduino para apagar láser
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
